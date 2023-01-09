84f5743f353dc8b2f7297c3be31f62aec5f4e40d
Callum Macdonald <git@callum-macdonald.com>
2023-01-09 12:10:57 +0100

    Icons, css baseline.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );

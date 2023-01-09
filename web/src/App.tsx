f8bb00057d3cae266631c1acd5028bd05ee92aea
Callum Macdonald <git@callum-macdonald.com>
2023-01-09 14:33:22 +0100

    Publish and fetch offers.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,14 +5,44 @@ import { useEffect, useState } from "react";
 import { getConfig } from "./services/config/config.service";
 import {
   getNostrProfile,
+  getOffers,
   NostrProfile,
+  Offer,
+  publishOffer,
   setNostrProfile,
 } from "./services/nostr/nostr.service";
+
+const addOfferFactory = (private_key: string) => () => {
+  const name = globalThis.prompt("Name of your hospex offer?");
+  const about = globalThis.prompt("Description of your offer?");
+  const picture = globalThis.prompt("URL to a picture of your offer?");
+  const location = globalThis.prompt(
+    "Please enter a plus code to identify the location of your offer"
+  );
+
+  if (name === null || about === null || picture == null || location === null) {
+    globalThis.alert(
+      "Sorry, something failed. Please provide all 3 values. #PCXaE2"
+    );
+    return;
+  }
+
+  const offer = {
+    name,
+    about,
+    picture,
+    location,
+  };
+
+  publishOffer(private_key, offer);
+};
 
 function App() {
   const { private_key } = getConfig();
   const [profile, setProfile] = useState<NostrProfile>({});
   const [profileError, setProfileError] = useState("");
+
+  const [offers, setOffers] = useState<Offer[]>([]);
 
   useEffect(() => {
     getNostrProfile(getPublicKey(private_key))
@@ -24,19 +54,33 @@ function App() {
       });
   }, []);
 
+  useEffect(() => {
+    getOffers().then((offers) => {
+      setOffers(offers);
+    });
+  }, []);
+
   return (
     <div className="App">
       <Container maxWidth="lg">
+        <Typography variant="h1">Gifts on nostr</Typography>
+        <Typography variant="h2">Keys</Typography>
         <Typography>Your private key is {private_key}</Typography>
         <Typography>Your public key is {getPublicKey(private_key)}</Typography>
+        <Typography variant="h2">Profile</Typography>
         {typeof profile?.name === "string" ? (
-          <div>{JSON.stringify(profile)}</div>
+          <div>
+            <Typography>Name: {profile.name}</Typography>
+            <Typography>About: {profile.about}</Typography>
+            <Typography>Picture: {profile.picture}</Typography>
+          </div>
         ) : profileError === "" ? (
           <div>Profile loading</div>
         ) : (
           <div>{profileError}</div>
         )}
         <Button
+          variant="contained"
           onClick={async () => {
             const name = globalThis.prompt("Name?");
             const about = globalThis.prompt("About?");
@@ -44,7 +88,7 @@ function App() {
 
             if (name === null || about === null || picture == null) {
               globalThis.alert(
-                "Sorry, something failed. Please provide all 3 values."
+                "Sorry, something failed. Please provide all 3 values. #QdnKpN"
               );
               return;
             }
@@ -57,6 +101,19 @@ function App() {
           }}
         >
           Set profile
+        </Button>
+        <Typography variant="h2">Offers</Typography>
+        <div>
+          <ul>
+            {offers.map((offer, i) => (
+              <li key={i}>
+                {offer.name}, {offer.about}, {offer.location}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Button variant="contained" onClick={addOfferFactory(private_key)}>
+          Add hospex offer
         </Button>
       </Container>
     </div>
--- web/src/services/nostr/nostr.service.ts
+++ web/src/services/nostr/nostr.service.ts
@@ -1,11 +1,12 @@
 import {
   Event,
+  Filter,
   getEventHash,
   getPublicKey,
   relayInit,
   signEvent,
 } from "nostr-tools";
-import { RELAY } from "../../constants";
+import { OFFER_D, OFFER_KIND, RELAY } from "../../constants";
 import { promiseWithTimeout } from "../promises/promises.service";
 
 const relay = relayInit(RELAY);
@@ -30,23 +31,19 @@ export type NostrProfile = {
   picture?: string;
 };
 
-export const getNostrProfile = (publicKey: string) => {
+const getEvents = (filter: Filter): Promise<Event[]> => {
   return promiseWithTimeout(() => {
     return new Promise((resolve, reject) => {
+      let results: Event[] = [];
       after.then(() => {
-        const sub = relay.sub([
-          {
-            kinds: [0],
-            authors: [publicKey],
-          },
-        ]);
+        const sub = relay.sub([filter]);
         sub.on("event", (event: any) => {
-          const profile = JSON.parse(event.content);
-          resolve(profile);
-          console.log("#AjDclA Got event for getProfile subscription", event);
+          results.push(event);
+          console.log("#AjDclA Got event for subscription", filter, event);
         });
         sub.on("eose", () => {
-          console.log("#3sVIaQ Got eose for getProfile subscription");
+          console.log("#3sVIaQ Got eose for subscription", filter);
+          resolve(results);
           sub.unsub();
         });
       });
@@ -54,27 +51,41 @@ export const getNostrProfile = (publicKey: string) => {
   }, 3e3);
 };
 
-export const setNostrProfile = (
-  privateKey: string,
-  profile: Required<NostrProfile>
-) => {
+export const getNostrProfile = (publicKey: string) => {
+  return getEvents({
+    kinds: [0],
+    authors: [publicKey],
+  }).then((events) => {
+    const [event] = events;
+    const profile = globalThis.JSON.parse(event.content);
+    return profile;
+  });
+};
+
+export const getOffers = () => {
+  return getEvents({
+    kinds: [OFFER_KIND],
+    "#d": [OFFER_D],
+  }).then((events) => {
+    const offers = events.map((event) => {
+      const content = globalThis.JSON.parse(event.content);
+      const [, location] = event.tags.find((tag) => tag[0] === "l");
+      return { ...content, location };
+    });
+    return offers;
+  });
+};
+
+const publishEvent = (privateKey: string, event: Event) => {
   return promiseWithTimeout(() => {
     return new Promise<void>((resolve, reject) => {
       after.then(() => {
-        const pubkey = getPublicKey(privateKey);
-        const event: Event = {
-          kind: 0,
-          created_at: Math.floor(Date.now() / 1e3),
-          tags: [],
-          content: globalThis.JSON.stringify(profile),
-          pubkey,
-        };
         event.id = getEventHash(event);
         event.sig = signEvent(event, privateKey);
 
         const pub = relay.publish(event);
 
-        console.log("#ahrp6D Publishing event", privateKey, pubkey, event);
+        console.log("#ahrp6D Publishing event", event);
 
         pub.on("ok", () => {
           console.log("#SzkjRB Event published");
@@ -87,10 +98,44 @@ export const setNostrProfile = (
         });
 
         pub.on("failed", (reason: any) => {
-          console.error("#MYiGu0 setNostrProfile() failed", reason);
+          console.error("#MYiGu0 publishing event failed", reason);
           reject(reason);
         });
       });
     });
   }, 2e3);
+};
+
+export const setNostrProfile = (
+  privateKey: string,
+  profile: Required<NostrProfile>
+) => {
+  const pubkey = getPublicKey(privateKey);
+  const event: Event = {
+    kind: 0,
+    created_at: Math.floor(Date.now() / 1e3),
+    tags: [],
+    content: globalThis.JSON.stringify(profile),
+    pubkey,
+  };
+  return publishEvent(privateKey, event);
+};
+
+export type Offer = Required<NostrProfile> & {
+  location: string;
+};
+export const publishOffer = (privateKey: string, offer: Offer) => {
+  const pubkey = getPublicKey(privateKey);
+  const { location, ...content } = offer;
+  const event: Event = {
+    kind: OFFER_KIND,
+    created_at: Math.floor(Date.now() / 1e3),
+    tags: [
+      ["d", OFFER_D],
+      ["l", location],
+    ],
+    content: globalThis.JSON.stringify(content),
+    pubkey,
+  };
+  return publishEvent(privateKey, event);
 };

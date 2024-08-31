a550f2be56b592c29740cd576dc827dad494acaf
Callum Macdonald <git@callum-macdonald.com>
2024-08-31 13:36:50 +0200

    Show a marker

--- nrapp/app/(tabs)/index.tsx
+++ nrapp/app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, View } from "react-native";
 
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 export default function HomeScreen() {
   return (
     <View style={styles.mapContainer}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map}>
+        <Marker coordinate={{ latitude: 52, longitude: 13 }} title="A marker" />
+      </MapView>
     </View>
   );
 }

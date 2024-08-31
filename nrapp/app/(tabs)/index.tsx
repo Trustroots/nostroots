dfa308dcf5da11fb6d1e4abd3d3598b8cc47b3c4
Callum Macdonald <git@callum-macdonald.com>
2024-08-31 13:33:21 +0200

    Remove more cruft

--- nrapp/app/(tabs)/index.tsx
+++ nrapp/app/(tabs)/index.tsx
@@ -1,35 +1,16 @@
 import { StyleSheet, View } from "react-native";
 
-import { ThemedView } from "@/components/ThemedView";
 import MapView from "react-native-maps";
 
 export default function HomeScreen() {
   return (
-    <ThemedView style={styles.titleContainer}>
-      <View style={styles.mapContainer}>
-        <MapView style={styles.map} />
-      </View>
-    </ThemedView>
+    <View style={styles.mapContainer}>
+      <MapView style={styles.map} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
   mapContainer: {
     flex: 1,
   },

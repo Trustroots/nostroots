d03ee25a5f201af29443c7a5bcac318e684207ba
Callum Macdonald <git@callum-macdonald.com>
2024-10-15 15:51:27 +0200

    Basic saga setup.

---
+++ nr-app/redux/mapSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./store";
+
+export const SLICE_NAME = "map";
+
+interface MapState {
+  visiblePlusCodes: string[];
+}
+
+const initialState: MapState = {
+  visiblePlusCodes: [],
+};
+
+const mapSlice = createSlice({
+  name: SLICE_NAME,
+  initialState,
+  reducers: {
+    setVisiblePlusCodes: (state, action: PayloadAction<string[]>) => {
+      state.visiblePlusCodes = action.payload;
+    },
+  },
+});
+
+export default mapSlice.reducer;
+
+export const { setVisiblePlusCodes } = mapSlice.actions;
+
+export const mapSelectors = mapSlice.getSelectors(
+  (state: RootState) => state[SLICE_NAME],
+);

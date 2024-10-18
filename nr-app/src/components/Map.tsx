218759e8c3f21342f84d9f5daa986465e61572e2
K <k@guaka.org>
2024-10-18 14:40:16 +0100

    focus on TextInput when adding note

--- nr-app/src/components/Map.tsx
+++ nr-app/src/components/Map.tsx
@@ -37,7 +37,16 @@ export default function Map() {
 
   const handleAddNote = () => {
     // Logic to add the note to the event or state
-    console.log("Note added:", note, "at", selectedCoordinate);
+    const plusCode = coordinatesToPlusCode(selectedCoordinate);
+    console.log(
+      "#a9vi49v Note added:",
+      note,
+      "at",
+      selectedCoordinate,
+      plusCode,
+      "plusCode not very precise",
+    );
+
     setModalVisible(false);
     setNote("");
   };
@@ -100,6 +109,11 @@ export default function Map() {
       >
         <View style={styles.modalContainer}>
           <TextInput
+            ref={(input) => {
+              if (input) {
+                input.focus();
+              }
+            }}
             style={styles.input}
             placeholder="Enter your note"
             value={note}

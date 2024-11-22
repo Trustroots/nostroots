23ce787a6d5f4f33af787088392ec0f1d847702a
K <k@guaka.org>
2024-11-22 18:19:42 +0000

    adding hitchwiki notes

--- nr-app/src/common/constants.ts
+++ nr-app/src/common/constants.ts
@@ -9,6 +9,9 @@ export const NOSTROOTS_VALIDATION_PUBKEY =
 export const HITCHMAPS_AUTHOR_PUBLIC_KEY =
   "53055ee011e96a00a705b38253b9cbc6614ccbd37df4dad42ec69bbe608c4209" as const;
 
+export const HITCHWIKI_AUTHOR_PUBLIC_KEY =
+  "16db5234c1dd8082897bd2d21bbec4b8051d2cd03e24b819aa5232077d443da9" as const;
+
 export const TIMESAFARI_AUTHOR_PUBLIC_KEY =
   "76e88d2e653fc3655f8e0b97f6bc85f5468eaffc5d64522b584ce13eedbd8af7" as const;
 
@@ -28,6 +31,13 @@ export const MAP_LAYERS: { [key in MAP_LAYER_KEY]: MapLayer } = {
     kind: 30399,
     pubkey: HITCHMAPS_AUTHOR_PUBLIC_KEY,
     markerColor: "gold",
+  },
+  hitchwiki: {
+    title: "Hitchwiki",
+    rootUrl: "https://hitchwiki.org",
+    kind: 30399,
+    pubkey: HITCHWIKI_AUTHOR_PUBLIC_KEY,
+    markerColor: "yellow",
   },
   timesafari: {
     title: "Time Safari",

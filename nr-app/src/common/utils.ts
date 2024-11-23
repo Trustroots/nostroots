c68f0072ebe573d23d73ac986d06427579e966a1
Callum Macdonald <git@callum-macdonald.com>
2024-11-23 15:31:01 +0000

    Bugfix. Don't expand strings.

--- nr-app/src/common/utils.ts
+++ nr-app/src/common/utils.ts
@@ -117,7 +117,11 @@ export function createLabelTags(
 ) {
   const tags = [
     ["L", labelName],
-    ["l", ...labelValue, labelName],
+    [
+      "l",
+      ...(Array.isArray(labelValue) ? labelValue : [labelValue]),
+      labelName,
+    ],
   ];
   return tags;
 }

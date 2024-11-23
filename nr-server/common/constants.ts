0a9e1bd4f5503275683d66157423f836a5c428ee
Callum Macdonald <git@callum-macdonald.com>
2024-11-23 14:23:13 +0000

    Validate 30397 events.

--- nr-server/common/constants.ts
+++ nr-server/common/constants.ts
@@ -3,7 +3,7 @@ export const RELAYS_STORAGE_KEY = "__nostrRelays" as const;
 export const PLUS_CODE_TAG_KEY = "l" as const;
 export const LABEL_NAMESPACE_TAG = "L";
 export const OPEN_LOCATION_CODE_NAMESPACE_TAG = "open-location-code";
-export const MAP_NOTE_KIND = 397;
+export const MAP_NOTE_KIND = 30397;
 export const MAP_NOTE_REPOST_KIND = 30398;
 export const DEFAULT_RELAYS = [
   "wss://relay.damus.io",

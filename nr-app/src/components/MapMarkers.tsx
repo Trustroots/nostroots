8af0baffbcadd45b819d6b8e13a1ce20aecf7557
Simon <abeluga@posteo.net>
2025-02-10 15:45:26 +0100

    only use google maps on android

--- nr-app/src/components/MapMarkers.tsx
+++ nr-app/src/components/MapMarkers.tsx
@@ -11,7 +11,7 @@ import { MAP_LAYER_KEY, MAP_LAYERS } from "@trustroots/nr-common";
 import { createSelector } from "@reduxjs/toolkit";
 import { matchFilter } from "nostr-tools";
 import { Fragment, useMemo } from "react";
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 import MapView, {
   Details,
   LongPressEvent,
@@ -77,8 +77,10 @@ export function MapMarkers() {
       pitchEnabled={false}
       onLongPress={handleMapLongPress}
       onRegionChangeComplete={handleMapRegionChange}
+      // only use google maps on android dev and prod builds
       provider={
-        Constants.executionEnvironment === ExecutionEnvironment.StoreClient
+        Constants.executionEnvironment === ExecutionEnvironment.StoreClient ||
+        Platform.OS !== "android"
           ? PROVIDER_DEFAULT
           : PROVIDER_GOOGLE
       }

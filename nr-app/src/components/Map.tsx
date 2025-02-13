bffda3143fa6d937e3f03cb7879f8761014497de
Callum Macdonald <git@callum-macdonald.com>
2025-02-13 14:00:52 +0100

    WIP: Add picker to modal.

--- nr-app/src/components/Map.tsx
+++ nr-app/src/components/Map.tsx
@@ -1,67 +1,15 @@
 import { FlatList, StyleSheet, Switch, Text, View } from "react-native";
 
-import { publishNotePromiseAction } from "@/redux/actions/publish.actions";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { mapActions, mapSelectors } from "@/redux/slices/map.slice";
-import { coordinatesToPlusCode } from "@/utils/map.utils";
 import { MAP_LAYER_KEY, MAP_LAYERS, MapLayer } from "@trustroots/nr-common";
-import React, { useMemo, useState } from "react";
-import { Button, Modal, TextInput } from "react-native";
-import Toast from "react-native-root-toast";
+import React from "react";
 import { MapMarkers } from "./MapMarkers";
+import MapModal from "./MapModal";
 
 export default function Map() {
   const enabledLayers = useAppSelector(mapSelectors.selectEnabledLayers);
-  const selectedCoordinate = useAppSelector(mapSelectors.selectSelectedLatLng);
-  const isAddNoteModalOpen = useAppSelector(
-    mapSelectors.selectIsAddNoteModalOpen,
-  );
   const dispatch = useAppDispatch();
-
-  const [noteContent, setNoteContent] = useState("");
-
-  const closeModal = useMemo(
-    () => () => dispatch(mapActions.closeAddNoteModal()),
-    [dispatch],
-  );
-
-  const handleAddNote = useMemo(
-    () =>
-      async function handleAddNoteHandler() {
-        if (selectedCoordinate === undefined) {
-          console.warn(
-            "#BerlDy Trying to add note without selectedCoordinate.",
-          );
-          return;
-        }
-
-        const plusCode = coordinatesToPlusCode(selectedCoordinate);
-        __DEV__ &&
-          console.log(
-            "#a9vi49v handleNoteAdd()",
-            noteContent,
-            "at",
-            selectedCoordinate,
-            plusCode,
-            "plusCode not very precise",
-          );
-
-        try {
-          await dispatch(publishNotePromiseAction(noteContent, plusCode));
-        } catch (error) {
-          Toast.show(
-            `Error: #dxmsa3 ${error instanceof Error ? error.message : "unknown"}`,
-            {
-              duration: Toast.durations.LONG,
-              position: Toast.positions.TOP,
-            },
-          );
-        }
-
-        dispatch(mapActions.closeAddNoteModal());
-      },
-    [selectedCoordinate, noteContent, dispatch],
-  );
 
   __DEV__ && console.log("#iNicG9 Map.tsx / render()", Date.now());
 
@@ -84,29 +32,7 @@ export default function Map() {
         />
       </View>
 
-      <Modal
-        visible={isAddNoteModalOpen}
-        transparent={true}
-        animationType="slide"
-        onRequestClose={closeModal}
-      >
-        <View style={styles.modalContainer}>
-          <TextInput
-            ref={(input) => {
-              if (input) {
-                input.focus();
-              }
-            }}
-            style={styles.input}
-            placeholder="Enter your note"
-            value={noteContent}
-            onChangeText={setNoteContent}
-            onSubmitEditing={handleAddNote}
-          />
-          <Button title="Add Note" onPress={handleAddNote} />
-          <Button title="Cancel" onPress={closeModal} />
-        </View>
-      </Modal>
+      <MapModal />
     </View>
   );
 }
@@ -126,23 +52,8 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
   },
-  modalContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.7)",
-  },
   layerToggle: {
     color: "rgba(255,255,255,1)",
     backgroundColor: "rgba(10, 10, 0, 0.2)",
-  },
-  input: {
-    width: 200,
-    padding: 10,
-    backgroundColor: "white",
-    marginBottom: 10,
-  },
-  marker: {
-    width: 200,
   },
 });

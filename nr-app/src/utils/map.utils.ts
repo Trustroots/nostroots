b41fe2c7c1bbd7a75fca3da8d745688ca7a9422e
Callum Macdonald <git@callum-macdonald.com>
2025-04-15 15:49:19 +0200

    Add a couple of helpers. More tests.

--- nr-app/src/utils/map.utils.ts
+++ nr-app/src/utils/map.utils.ts
@@ -1,16 +1,33 @@
 import { NOSTR_EVENT_INDEX_MAXIMUM_PLUS_CODE_LENGTH } from "@/constants";
-import { MAP_LAYER_KEY, MAP_LAYERS, MapLayer } from "@trustroots/nr-common";
-import { getFirstTagValueFromEvent } from "@trustroots/nr-common";
+import {
+  MAP_LAYER_KEY,
+  MAP_LAYERS,
+  MapLayer,
+  getFirstTagValueFromEvent,
+} from "@trustroots/nr-common";
 import { NostrEvent } from "nostr-tools";
 import OpenLocationCode from "open-location-code-typescript";
 import { urlJoin } from "url-join-ts";
 
 type PlusCodeShortLength = 2 | 4 | 6 | 8;
 
-const plusCodeCharacters = "23456789CFGHJMPQRVWX" as const;
+const PLUS_CODE_CHARACTERS = "23456789CFGHJMPQRVWX" as const;
 
 export function isValidPlusCode(code: string): boolean {
   return OpenLocationCode.isValid(code);
+}
+
+export function plusCodeToFirstFourSegments(
+  plusCode: string,
+): [string, string, string, string] {
+  if (!isValidPlusCode(plusCode)) {
+    throw new Error("#yCmPga-invalid-plus-code");
+  }
+  const first = plusCode.substring(0, 2);
+  const second = plusCode.substring(2, 4);
+  const third = plusCode.substring(4, 6);
+  const fourth = plusCode.substring(6, 8);
+  return [first, second, third, fourth];
 }
 
 export function coordinatesToPlusCode({
@@ -110,6 +127,12 @@ export function plusCodeToRectangle(
   }
 }
 
+/**
+ * Get a set of plus codes that contains the entire space of the visible map.
+ *
+ * This is useful to fetch events. By fetching events for these plus codes the
+ * entire map will be covered and some area outside of the visible map.
+ */
 export function allPlusCodesForRegion({
   latitude,
   latitudeDelta,
@@ -164,28 +187,26 @@ export function allPlusCodesForRegion({
   const [bottomRow, leftColumn] = bottomLeftLastPair;
   const [topRow, rightColumn] = topRightLastPair;
 
-  const bottomRowIndex = plusCodeCharacters.indexOf(bottomRow);
-  const topRowIndex = plusCodeCharacters.indexOf(topRow);
-  const leftColumnIndex = plusCodeCharacters.indexOf(leftColumn);
-  const rightColumnIndex = plusCodeCharacters.indexOf(rightColumn);
+  const bottomRowIndex = PLUS_CODE_CHARACTERS.indexOf(bottomRow);
+  const topRowIndex = PLUS_CODE_CHARACTERS.indexOf(topRow);
+  const leftColumnIndex = PLUS_CODE_CHARACTERS.indexOf(leftColumn);
+  const rightColumnIndex = PLUS_CODE_CHARACTERS.indexOf(rightColumn);
 
   const rows = topRowIndex - bottomRowIndex + 1;
   const columns = rightColumnIndex - leftColumnIndex + 1;
 
   // Nested iteration
-  const parts = Array.from({ length: rows })
-    .map((empty, rowIndex) => {
-      return Array.from({ length: columns }).map((empty, columnIndex) => {
-        const outputRowIndex = bottomRowIndex + rowIndex;
-        const outputColumnIndex = leftColumnIndex + columnIndex;
+  const parts = Array.from({ length: rows }).flatMap((empty, rowIndex) => {
+    return Array.from({ length: columns }).map((empty, columnIndex) => {
+      const outputRowIndex = bottomRowIndex + rowIndex;
+      const outputColumnIndex = leftColumnIndex + columnIndex;
 
-        const rowCode = plusCodeCharacters[outputRowIndex];
-        const columnCode = plusCodeCharacters[outputColumnIndex];
+      const rowCode = PLUS_CODE_CHARACTERS[outputRowIndex];
+      const columnCode = PLUS_CODE_CHARACTERS[outputColumnIndex];
 
-        return [rowCode, columnCode];
-      });
-    })
-    .flat();
+      return [rowCode, columnCode];
+    });
+  });
 
   const codePrefixLength = outputCodeLength > 2 ? outputCodeLength - 2 : 0;
   const codePrefix = bottomLeftCode.slice(0, codePrefixLength);
@@ -197,6 +218,61 @@ export function allPlusCodesForRegion({
     return code;
   });
   return codes;
+}
+
+export function plusCodeHasTrailingZeroes(plusCode: string) {
+  // A plus code cannot have trailing zeroes unless it is exactly 9 chars long
+  if (plusCode.length !== 9) {
+    return false;
+  }
+
+  if (!isValidPlusCode(plusCode)) {
+    return false;
+  }
+
+  const secondPair = plusCode.substring(2, 4);
+  const thirdPair = plusCode.substring(4, 6);
+  const fourthPair = plusCode.substring(6, 8);
+
+  if (
+    (secondPair === "00" && thirdPair === "00" && fourthPair === "00") ||
+    (thirdPair === "00" && fourthPair === "00") ||
+    fourthPair === "00"
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+export function getAllChildPlusCodes(plusCode: string) {
+  if (!plusCodeHasTrailingZeroes(plusCode)) {
+    throw new Error("#g4qh7N-invalid-plus-code");
+  }
+
+  const segments = plusCodeToFirstFourSegments(plusCode);
+
+  const nonZeroPrefix = segments.reduce((output, segment) => {
+    if (segment === "00") {
+      return output;
+    }
+    return output + segment;
+  }, "");
+
+  const everySubSegment = Array.from(PLUS_CODE_CHARACTERS).flatMap(
+    (firstCharacter) =>
+      Array.from(PLUS_CODE_CHARACTERS).map(
+        (secondCharacter) => firstCharacter + secondCharacter,
+      ),
+  );
+
+  const childPlusCodes = everySubSegment.map((subSegment) => {
+    const prefix = nonZeroPrefix + subSegment;
+    const plusCode = prefix.padEnd(8, "0") + "+";
+    return plusCode;
+  });
+
+  return childPlusCodes;
 }
 
 export function getMapLayer(layerKey?: string) {

b75c5070dc75e1960ff6082a44cd25cde202ef77
Callum Macdonald <git@callum-macdonald.com>
2025-04-18 21:44:30 +0200

    Group notes by kind.

--- nr-app/app/(tabs)/list.tsx
+++ nr-app/app/(tabs)/list.tsx
@@ -5,9 +5,14 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { useAppSelector } from "@/redux/hooks";
 import { eventsSelectors } from "@/redux/slices/events.slice";
+import { A, D } from "@mobily/ts-belt";
 
 export default function TabTwoScreen() {
-  const events = useAppSelector(eventsSelectors.selectAll);
+  const allEvents = useAppSelector(eventsSelectors.selectAll);
+  const groupedEvents = Object.entries(
+    A.groupBy(allEvents, (event) => event.event.kind.toString()),
+  );
+  // const groupedEvents = events.reduce((eventWithMetadata) => {}, {});
 
   return (
     <SafeAreaView>
@@ -17,21 +22,28 @@ export default function TabTwoScreen() {
         </ThemedView>
         <View>
           <Text style={styles.note}>
-            We have a total of {events.length} notes.
+            We have a total of {allEvents.length} notes.
           </Text>
-          {events.map((event) => (
-            <View key={event.event.id}>
-              <Text style={styles.note}>{event.event.content}</Text>
-              <Text style={styles.note}>
-                {new Date(event.event.created_at * 1000).toLocaleString()}
-              </Text>
-              <Text style={styles.note}>{event.event.pubkey}</Text>
-              <Text style={styles.note}>
-                {Array.isArray(event.event.tags[1]) && event.event.tags[1][1]
-                  ? event.event.tags[1][1]
-                  : null}
-              </Text>
-              <Text style={styles.note}>----------------------</Text>
+          {groupedEvents.map(([kind, events]) => (
+            <View key={kind}>
+              <Text style={styles.kindHeading}>Kind: {kind}</Text>
+              <View style={styles.marker} />
+              {events.map((event) => (
+                <View key={event.event.id}>
+                  <Text style={styles.note}>{event.event.content}</Text>
+                  <Text style={styles.note}>
+                    {new Date(event.event.created_at * 1000).toLocaleString()}
+                  </Text>
+                  <Text style={styles.note}>{event.event.pubkey}</Text>
+                  <Text style={styles.note}>
+                    {Array.isArray(event.event.tags[1]) &&
+                    event.event.tags[1][1]
+                      ? event.event.tags[1][1]
+                      : null}
+                  </Text>
+                  <Text style={styles.note}>----------------------</Text>
+                </View>
+              ))}
             </View>
           ))}
         </View>
@@ -47,5 +59,13 @@ const styles = StyleSheet.create({
   },
   note: {
     color: "#008800",
+  },
+  kindHeading: {
+    fontSize: 30,
+  },
+  marker: {
+    width: "100%",
+    height: 2,
+    backgroundColor: "black",
   },
 });

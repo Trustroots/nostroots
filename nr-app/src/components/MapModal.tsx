3c2d997e41594e2d9ffcde252b4438a59f4929e7
Callum Macdonald <git@callum-macdonald.com>
2025-04-18 14:19:14 +0200

    Close modal.

--- nr-app/src/components/MapModal.tsx
+++ nr-app/src/components/MapModal.tsx
@@ -1,14 +1,21 @@
-import { useAppSelector } from "@/redux/hooks";
-import { mapSelectors } from "@/redux/slices/map.slice";
-import { Modal, Text, View } from "react-native";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { mapActions, mapSelectors } from "@/redux/slices/map.slice";
+import { Button, Modal, Text, View } from "react-native";
 
 export default function MapModal() {
+  const dispatch = useAppDispatch();
   const showModal = useAppSelector(mapSelectors.selectIsMapModalOpen);
 
   return (
     <Modal visible={showModal}>
       <View>
         <Text>Modal is under development</Text>
+        <Button
+          title="Close"
+          onPress={() => {
+            dispatch(mapActions.closeMapModal());
+          }}
+        />
       </View>
     </Modal>
   );

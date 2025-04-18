2fd4cde318f26472052d7eaae6110511cac0140d
Callum Macdonald <git@callum-macdonald.com>
2025-04-18 19:20:21 +0200

    Wrap modal in a safe area view.

--- nr-app/src/components/MapModal.tsx
+++ nr-app/src/components/MapModal.tsx
@@ -3,6 +3,7 @@ import { mapActions, mapSelectors } from "@/redux/slices/map.slice";
 import { Button, Modal, ScrollView, Text, View } from "react-native";
 import NotesList from "./NotesList";
 import AddNoteForm from "./AddNoteForm";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function MapModal() {
   const dispatch = useAppDispatch();
@@ -12,29 +13,31 @@ export default function MapModal() {
 
   return (
     <Modal visible={showModal}>
-      <ScrollView>
-        <Button
-          title="Close"
-          onPress={() => {
-            dispatch(mapActions.closeMapModal());
-          }}
-        />
-        <NotesList plusCode={selectedPlusCode} />
-        {selectedLayer === "trustroots" ? (
-          <AddNoteForm />
-        ) : (
-          <View>
-            <Text>Choose the trustroots layer to be able to add content</Text>
-          </View>
-        )}
-        <Text>Modal is under development</Text>
-        <Button
-          title="Close"
-          onPress={() => {
-            dispatch(mapActions.closeMapModal());
-          }}
-        />
-      </ScrollView>
+      <SafeAreaView>
+        <ScrollView>
+          <Button
+            title="Close"
+            onPress={() => {
+              dispatch(mapActions.closeMapModal());
+            }}
+          />
+          <NotesList plusCode={selectedPlusCode} />
+          {selectedLayer === "trustroots" ? (
+            <AddNoteForm />
+          ) : (
+            <View>
+              <Text>Choose the trustroots layer to be able to add content</Text>
+            </View>
+          )}
+          <Text>Modal is under development</Text>
+          <Button
+            title="Close"
+            onPress={() => {
+              dispatch(mapActions.closeMapModal());
+            }}
+          />
+        </ScrollView>
+      </SafeAreaView>
     </Modal>
   );
 }

df99f370758c84c22646ea71fd5dc9eacdd1f0cc
Callum Macdonald <git@callum-macdonald.com>
2025-11-07 18:34:54 +0000

    Linting.

--- nr-app/src/components/ui/native-only-animated-view.tsx
+++ nr-app/src/components/ui/native-only-animated-view.tsx
@@ -1,5 +1,5 @@
-import { Platform } from 'react-native';
-import Animated from 'react-native-reanimated';
+import { Platform } from "react-native";
+import Animated from "react-native-reanimated";
 
 /**
  * This component is used to wrap animated views that should only be animated on native.
@@ -11,9 +11,10 @@ import Animated from 'react-native-reanimated';
  * </NativeOnlyAnimatedView>
  */
 function NativeOnlyAnimatedView(
-  props: React.ComponentProps<typeof Animated.View> & React.RefAttributes<Animated.View>
+  props: React.ComponentProps<typeof Animated.View> &
+    React.RefAttributes<Animated.View>,
 ) {
-  if (Platform.OS === 'web') {
+  if (Platform.OS === "web") {
     return <>{props.children as React.ReactNode}</>;
   } else {
     return <Animated.View {...props} />;

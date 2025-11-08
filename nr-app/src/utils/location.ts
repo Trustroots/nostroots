74966eb33fff50cb123da3234587e778039a18a3
awongh <akira@awongh.com>
2025-11-08 13:38:24 +0000

    take out console.logs for performance

--- nr-app/src/utils/location.ts
+++ nr-app/src/utils/location.ts
@@ -3,7 +3,7 @@ import * as Location from "expo-location";
 export async function getCurrentLocation() {
   let { status } = await Location.requestForegroundPermissionsAsync();
   if (status !== "granted") {
-    console.error("Permission to access location was denied");
+    // console.error("Permission to access location was denied");
     return null;
   }
 

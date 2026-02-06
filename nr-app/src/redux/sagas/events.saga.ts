bdac3c40944fd743c39b8ceed7973aef2c7ad6b4
Callum Macdonald <git@callum-macdonald.com>
2026-02-06 14:32:53 +0100

    Call flush on startup as well. fix #145

--- nr-app/src/redux/sagas/events.saga.ts
+++ nr-app/src/redux/sagas/events.saga.ts
@@ -6,18 +6,22 @@ import { getCurrentTimestamp } from "@trustroots/nr-common";
 
 const FLUSH_EXPIRED_EVENTS_EVERY_MILLISECONDS = 10 * 60 * 1000;
 
+function flushExpiredEvents() {
+  store.dispatch(
+    eventsActions.flushExpiredEvents({
+      currentTimestampSeconds: getCurrentTimestamp(),
+    }),
+  );
+}
+
 export function startPeriodicFlushingOfExpiredEvents() {
-  setInterval(() => {
-    store.dispatch(
-      eventsActions.flushExpiredEvents({
-        currentTimestampSeconds: getCurrentTimestamp(),
-      }),
-    );
-  }, FLUSH_EXPIRED_EVENTS_EVERY_MILLISECONDS);
+  flushExpiredEvents();
+  setInterval(flushExpiredEvents, FLUSH_EXPIRED_EVENTS_EVERY_MILLISECONDS);
 }
 
 export function* flushExpiredEventsSaga() {
   yield take(rehydrated);
+  startPeriodicFlushingOfExpiredEvents();
 }
 
 export function* eventsSaga() {

32acebeb6b0d0def842e0a1eb9741dc63089f1a0
Simon <abeluga@posteo.net>
2026-02-24 14:13:34 +0100

    slightly more helpful error

--- nr-app/src/redux/sagas/publish.saga.ts
+++ nr-app/src/redux/sagas/publish.saga.ts
@@ -63,7 +63,8 @@ export function* publishEventTemplateSagaEffect(
     yield put(publishEventTemplatePromiseAction.success(payload));
     resolvePromiseAction(action, payload);
   } catch (error) {
-    if (__DEV__) console.error("#oUtVSG Got error", error);
+    if (__DEV__)
+      console.error("#oUtVSG Got error while publishing event", error);
     const serializableError = getSerializableError(error);
     yield put(publishEventTemplatePromiseAction.failure(serializableError));
     rejectPromiseAction(action, error);
